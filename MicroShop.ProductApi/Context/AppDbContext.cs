6b893473aa31b2c16f263b813b58f003aa5e3eae
MarcoM3l0 <josemarcomelonascimento@gmail.com>
2025-11-03 17:33:06 -0300

    chore: configurar EF Core com Fluent API e dados de teste no ProductApi
    - Sobrescritas as convenções padrão do EF Core usando Fluent API
    - Configurado mapeamento detalhado para os models Product e Category
    - Populadas as tabelas com dados iniciais para testes futuros

--- MicroShop.ProductApi/Context/AppDbContext.cs
+++ MicroShop.ProductApi/Context/AppDbContext.cs
@@ -12,4 +12,53 @@ public class AppDbContext : DbContext
 
     public DbSet<Product> Products { get; set; }
     public DbSet<Category> Categories { get; set; }
+
+    // Fluent API
+    protected override void OnModelCreating(ModelBuilder mb)
+    {
+        // Category
+        mb.Entity<Category>()
+     .HasKey(c => c.CategoryId);
+
+        mb.Entity<Category>()
+            .Property(c => c.Name)
+            .IsRequired()
+            .HasMaxLength(100);
+
+        // Product
+        mb.Entity<Product>()
+            .HasKey(p => p.ProductId);
+
+        mb.Entity<Product>()
+            .Property(p => p.Name)
+            .IsRequired()
+            .HasMaxLength(100);
+
+        mb.Entity<Product>()
+            .Property(p => p.Description)
+            .IsRequired()
+            .HasMaxLength(255);
+
+        mb.Entity<Product>()
+            .Property(p => p.ImageURL)
+            .IsRequired()
+            .HasMaxLength(255);
+
+        mb.Entity<Product>()
+            .Property(p => p.Price)
+            .HasPrecision(12, 2);
+
+        mb.Entity<Category>()
+            .HasMany(c => c.Products)
+            .WithOne(p => p.Category)
+            .IsRequired()
+            .OnDelete(DeleteBehavior.Cascade);
+
+        // Seed Data
+        mb.Entity<Category>().HasData(
+            new Category { CategoryId = 1, Name = "Material Escolar" },
+            new Category { CategoryId = 2, Name = "Eletrônicos" }
+        );
+
+    }
 }
