0d783b69534e0c9917fb739398e61ea9504bf910
MarcoM3l0 <josemarcomelonascimento@gmail.com>
2025-11-07 18:28:13 -0300

    chore: configurar JSON para ignorar ciclos de referência no ProductApi
    - Adicionada configuração AddJsonOptions no AddControllers
    - Definido ReferenceHandler.IgnoreCycles para evitar erros de serialização

--- MicroShop.ProductApi/Program.cs
+++ MicroShop.ProductApi/Program.cs
@@ -4,13 +4,13 @@ using MicroShop.ProductApi.Repositories;
 using MicroShop.ProductApi.Services.concrete;
 using MicroShop.ProductApi.Services.interfaces;
 using Microsoft.EntityFrameworkCore;
+using System.Text.Json.Serialization;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
 
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddControllers().AddJsonOptions(x =>
+            x.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles);
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
