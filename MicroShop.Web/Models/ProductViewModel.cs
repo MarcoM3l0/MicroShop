8f1a17071fe539c94f6f6c5c14b96d2e888f20cd
MarcoM3l0 <josemarcomelonascimento@gmail.com>
2025-12-05 14:50:08 -0300

    feat: implementar serviços e controlador MVC para produtos e categorias
    - Implementado ProductService e criado ICategoryService com sua classe concreta
    - Registrados serviços no Program.cs para injeção de dependência
    - Criado controlador MVC ProductsController para consumir ProductApi
    - Desenvolvida view para exibir resultados do controlador
    - Adicionado botão na view para criação de novos produtos

--- MicroShop.Web/Models/ProductViewModel.cs
+++ MicroShop.Web/Models/ProductViewModel.cs
@@ -1,13 +1,21 @@
-﻿namespace MicroShop.Web.Models;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MicroShop.Web.Models;
 
 public class ProductViewModel
 {
     public int ProductId { get; set; }
+    [Required]
     public string? Name { get; set; }
+    [Required]
     public decimal Price { get; set; }
+    [Required]
     public string? Description { get; set; }
+    [Required]
     public int Stock { get; set; }
+    [Required]
     public string? ImageUrl { get; set; }
     public string? CategoryName { get; set; }
+    [Display(Name = "Categoria")]
     public int CategoryId { get; set; }
 }
--- MicroShop.Web/Services/ProductService.cs
+++ MicroShop.Web/Services/ProductService.cs
@@ -1,5 +1,6 @@
 ﻿using MicroShop.Web.Models;
 using MicroShop.Web.Services.Contracts;
+using System.Text;
 using System.Text.Json;
 
 namespace MicroShop.Web.Services;
@@ -8,7 +9,7 @@ public class ProductService : IProductService
 {
     private readonly IHttpClientFactory _httpClientFactory;
     private readonly JsonSerializerOptions _options;
-    private const string apiEndpoint = "/api/products/";
+    private const string apiEndpoint = "https://localhost:7186/api/Products";
     private ProductViewModel productVM;
     private IEnumerable<ProductViewModel> productVMs;
 
@@ -22,29 +23,105 @@ public class ProductService : IProductService
         };
     }
 
-    public Task<IEnumerable<ProductViewModel>> GetAllProducts()
+    public async Task<IEnumerable<ProductViewModel>> GetAllProducts()
     {
-        throw new NotImplementedException();
+        var client = _httpClientFactory.CreateClient("ProductApi");
+
+        using (var response = await client.GetAsync(apiEndpoint))
+        {
+            if (response.IsSuccessStatusCode)
+            {
+                var apiResponse = await response.Content.ReadAsStreamAsync();
+                productVMs = JsonSerializer.Deserialize<IEnumerable<ProductViewModel>>(apiResponse, _options);
+                return productVMs;
+            }
+            else
+            {
+                return null;
+            }
+        }
     }
 
-    public Task<ProductViewModel> GetProductById(int productId)
+    public async Task<ProductViewModel> GetProductById(int productId)
     {
-        throw new NotImplementedException();
+        var client = _httpClientFactory.CreateClient("ProductApi");
+
+        using (var response = await client.GetAsync(apiEndpoint + productId))
+        {
+            if (response.IsSuccessStatusCode)
+            {
+                var apiResponse = await response.Content.ReadAsStreamAsync();
+                productVM = JsonSerializer.Deserialize<ProductViewModel>(apiResponse, _options);
+                return productVM;
+            }
+            else
+            {
+                return null;
+            }
+        }
     }
 
-    public Task<ProductViewModel> CreateProduct(ProductViewModel productVM)
+    public async Task<ProductViewModel> CreateProduct(ProductViewModel productVM)
     {
-        throw new NotImplementedException();
+        var client = _httpClientFactory.CreateClient("ProductApi");
+
+        StringContent content = new StringContent(
+                                            JsonSerializer.Serialize(productVM), 
+                                            Encoding.UTF8, "application/json");
+
+        using (var response = await client.PostAsync(apiEndpoint, content))
+        {
+            if (response.IsSuccessStatusCode)
+            {
+                var apiResponse = await response.Content.ReadAsStreamAsync();
+                productVM = await JsonSerializer
+                           .DeserializeAsync<ProductViewModel>(apiResponse, _options);
+                return productVM;
+            }
+            else
+            {
+                return null;
+            }
+        }
     }
 
-    public Task<ProductViewModel> UpdateProduct(ProductViewModel productVM)
+    public async Task<ProductViewModel> UpdateProduct(ProductViewModel productVM)
     {
-        throw new NotImplementedException();
+        var client = _httpClientFactory.CreateClient("ProductApi");
+
+        ProductViewModel updatedProduct = new ProductViewModel();
+
+        using (var response = await client.PutAsJsonAsync(apiEndpoint, productVM))
+        {
+            if (response.IsSuccessStatusCode)
+            {
+                var apiResponse = await response.Content.ReadAsStreamAsync();
+                updatedProduct = await JsonSerializer
+                           .DeserializeAsync<ProductViewModel>(apiResponse, _options);
+                return updatedProduct;
+            }
+            else
+            {
+                return null;
+            }
+        }
     }
 
-    public Task<bool> DeleteProduct(int productId)
+    public async Task<bool> DeleteProduct(int productId)
     {
-        throw new NotImplementedException();
+        var client = _httpClientFactory.CreateClient("ProductApi");
+
+        using (var response = await  client.DeleteAsync(apiEndpoint + productId))
+        {
+            if (response.IsSuccessStatusCode)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
     }
 
 }
